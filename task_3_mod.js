7362f494ec207dcdd5f7a410b2cd9c1a69ec761e
vitwits <vitwits@gmail.com>
2019-10-29 18:52:11 +0200

    mod

---
+++ task_3_mod.js
@@ -0,0 +1,221 @@
+'use strict'
+
+function cash(price, bill) {
+  const changeVariants = []
+  let sumRight = 0
+  let lastArray
+  let findPoint
+  let leftPart
+  let rightPart
+  let change = bill - price
+  const stack = []
+  let a
+  const billStack = [200, 100, 50, 20, 10, 5, 2, 1]
+
+  for (var i = 0; i < 1; i++) {
+    while (i < billStack.length) {
+      for (a = Math.floor(change / billStack[i]); a > 0; a--) {
+        change -= billStack[i]
+        stack.push(billStack[i])
+      }
+      i++;
+    }
+  }
+
+  changeVariants.push(stack)
+
+  for (i = 0; i >= 0; i++) {
+    lastArray = changeVariants[i]
+
+    if (changeVariants[i].length === bill - price) {
+      break
+    }
+
+    if (lastArray.includes(1, 0) === true) {
+      findPoint = lastArray.indexOf(1, 0)
+      findPoint = lastArray.indexOf(1, 0)
+      leftPart = lastArray.slice(0, findPoint - 1)
+      rightPart = lastArray.slice(findPoint - 1, lastArray.length)
+      rightPart.forEach(function (item) {
+        sumRight += item
+      })
+    } else {
+      findPoint = lastArray.length
+      leftPart = lastArray.slice(0, findPoint - 1)
+      rightPart = lastArray.length
+      sumRight = lastArray[lastArray.length - 1]
+    }
+
+
+    
+changeVariants.forEach(function(item){
+
+
+    
+    if (lastArray[findPoint - 1] === 2) {
+      for (a = Math.floor(sumRight / billStack[7]); a > 0; a--) {
+        sumRight -= billStack[7]
+        leftPart.push(billStack[7])
+      }
+      changeVariants.push(leftPart)
+    }
+
+    if (lastArray[findPoint - 1] === 5) {
+      for (a = Math.floor(sumRight / billStack[6]); a > 0; a--) {
+        sumRight -= billStack[6]
+        leftPart.push(billStack[6])
+      }
+
+      for (a = Math.floor(sumRight / billStack[7]); a > 0; a--) {
+        sumRight -= billStack[7]
+        leftPart.push(billStack[7])
+      }
+      changeVariants.push(leftPart)
+    }
+
+    if (lastArray[findPoint - 1] === 10) {
+      for (a = Math.floor(sumRight / billStack[5]); a > 0; a--) {
+        sumRight -= billStack[5]
+        leftPart.push(billStack[5])
+      }
+
+      for (a = Math.floor(sumRight / billStack[6]); a > 0; a--) {
+        sumRight -= billStack[6]
+        leftPart.push(billStack[6])
+      }
+
+      for (a = Math.floor(sumRight / billStack[7]); a > 0; a--) {
+        sumRight -= billStack[7]
+        leftPart.push(billStack[7])
+      }
+      changeVariants.push(leftPart)
+    }
+
+    if (lastArray[findPoint - 1] === 20) {
+      for (a = Math.floor(sumRight / billStack[4]); a > 0; a--) {
+        sumRight -= billStack[4]
+        leftPart.push(billStack[4])
+      }
+
+      for (a = Math.floor(sumRight / billStack[5]); a > 0; a--) {
+        sumRight -= billStack[5]
+        leftPart.push(billStack[5])
+      }
+
+      for (a = Math.floor(sumRight / billStack[6]); a > 0; a--) {
+        sumRight -= billStack[6]
+        leftPart.push(billStack[6])
+      }
+
+      for (a = Math.floor(sumRight / billStack[7]); a > 0; a--) {
+        sumRight -= billStack[7]
+        leftPart.push(billStack[7])
+      }
+      changeVariants.push(leftPart)
+    }
+
+    if (lastArray[findPoint - 1] === 50) {
+      for (a = Math.floor(sumRight / billStack[3]); a > 0; a--) {
+        sumRight -= billStack[3]
+        leftPart.push(billStack[3])
+      }
+
+      for (a = Math.floor(sumRight / billStack[4]); a > 0; a--) {
+        sumRight -= billStack[4]
+        leftPart.push(billStack[4])
+      }
+
+      for (a = Math.floor(sumRight / billStack[5]); a > 0; a--) {
+        sumRight -= billStack[5]
+        leftPart.push(billStack[5])
+      }
+
+      for (a = Math.floor(sumRight / billStack[6]); a > 0; a--) {
+        sumRight -= billStack[6]
+        leftPart.push(billStack[6])
+      }
+
+      for (a = Math.floor(sumRight / billStack[7]); a > 0; a--) {
+        sumRight -= billStack[7]
+        leftPart.push(billStack[7])
+      }
+      changeVariants.push(leftPart)
+    }
+
+    if (lastArray[findPoint - 1] === 100) {
+      for (a = Math.floor(sumRight / billStack[2]); a > 0; a--) {
+        sumRight -= billStack[2]
+        leftPart.push(billStack[2])
+      }
+
+      for (a = Math.floor(sumRight / billStack[3]); a > 0; a--) {
+        sumRight -= billStack[3]
+        leftPart.push(billStack[3])
+      }
+
+      for (a = Math.floor(sumRight / billStack[4]); a > 0; a--) {
+        sumRight -= billStack[4]
+        leftPart.push(billStack[4])
+      }
+
+      for (a = Math.floor(sumRight / billStack[5]); a > 0; a--) {
+        sumRight -= billStack[5]
+        leftPart.push(billStack[5])
+      }
+
+      for (a = Math.floor(sumRight / billStack[6]); a > 0; a--) {
+        sumRight -= billStack[6]
+        leftPart.push(billStack[6])
+      }
+
+      for (a = Math.floor(sumRight / billStack[7]); a > 0; a--) {
+        sumRight -= billStack[7]
+        leftPart.push(billStack[7])
+      }
+      changeVariants.push(leftPart)
+    }
+
+    if (lastArray[findPoint - 1] === 200) {
+      for (a = Math.floor(sumRight / billStack[1]); a > 0; a--) {
+        sumRight -= billStack[1]
+        leftPart.push(billStack[1])
+      }
+
+      for (a = Math.floor(sumRight / billStack[2]); a > 0; a--) {
+        sumRight -= billStack[2]
+        leftPart.push(billStack[2])
+      }
+
+      for (a = Math.floor(sumRight / billStack[3]); a > 0; a--) {
+        sumRight -= billStack[3]
+        leftPart.push(billStack[3])
+      }
+
+      for (a = Math.floor(sumRight / billStack[4]); a > 0; a--) {
+        sumRight -= billStack[4]
+        leftPart.push(billStack[4])
+      }
+
+      for (a = Math.floor(sumRight / billStack[5]); a > 0; a--) {
+        sumRight -= billStack[5]
+        leftPart.push(billStack[5])
+      }
+
+      for (a = Math.floor(sumRight / billStack[6]); a > 0; a--) {
+        sumRight -= billStack[6]
+        leftPart.push(billStack[6])
+      }
+
+      for (a = Math.floor(sumRight / billStack[7]); a > 0; a--) {
+        sumRight -= billStack[7]
+        leftPart.push(billStack[7])
+      }
+    
+      changeVariants.push(leftPart)
+    }
+  })
+  }
+  console.log(changeVariants)
+}
+
+cash(136, 200)
